4bd512e8a55e305b804b00e1a1781825995ea08e
Erdem Yilmaz <hackerdem@users.noreply.github.com>
2017-12-05 22:41:20 +1100

    improvements

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.core.urlresolvers import reverse
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
 class Post(models.Model):
     STATUS_CHOICES=(
         ('draft','Draft'),
-        ('publish','publish')
+        ('published','Published')
     )
     title=models.CharField(max_length=250)
     slug=models.SlugField(max_length=250,
@@ -16,11 +19,17 @@ class Post(models.Model):
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
     status=models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
-    
+    objects=models.Manager() # the default manager
+    published=PublishedManager() #the custom manager
     class Meta:
         ordering=('-publish',)
         
     def __str__(self):
         return self.title
-    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
 # Create your models here.
