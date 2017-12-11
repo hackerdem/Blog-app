3e6bfee1e7e1ebb7f8fe2fa96681934c349275f3
Erdem Yilmaz <hackerdem@users.noreply.github.com>
2017-12-11 17:10:59 +1100

    improvements

--- myblog/settings.py
+++ myblog/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER=
-EMAIL_HOST_PASSWORD=
+EMAIL_HOST_USER='**********@gmail.com'
+EMAIL_HOST_PASSWORD='**********'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
