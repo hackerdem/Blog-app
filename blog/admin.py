302fb4d4e8c55954b30d58caeb308564531017f9
Erdem Yilmaz <hackerdem@users.noreply.github.com>
2017-12-16 04:47:04 +1100

    improvements

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Post
-
+from .models import Post,Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -14,4 +13,11 @@ class PostAdmin(admin.ModelAdmin):
     
     
 admin.site.register(Post,PostAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=('name','email','post','created','active')
+    list_filter=('active','created','updated')
+    search_fields=('name','email','body')
+admin.site.register(Comment,CommentAdmin)
+
 # Register your models here.
