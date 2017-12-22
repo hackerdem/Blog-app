1b7ae623dae329616bbce57994442c24264d03cb
Erdem Yilmaz <hackerdem@users.noreply.github.com>
2017-12-22 21:10:54 +1100

    tag

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 class PostListView(ListView):
     queryset=Post.published.all()
@@ -60,3 +61,10 @@ def post_detail(request,year,month,day,post):
                   {'post':post,
                    'comments':comments,
                    'comment_form':comment_form})
+def post_list(request,tag_slug=None):
+    object_list=Post.published.all()
+    tag=None
+    
+    if tag_slug:
+        tag=get_object_or_404(Tag,slug=tag_slug)
+        object_list=object_list.filter(tags__in=[tag])
